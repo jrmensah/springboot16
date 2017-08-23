aaa18e2c210072b3cf4ff762fc03b0357dd48639
jrmensah <jmensah4@student.umuc.edu>
2017-08-23 13:43:49 -0400

    First commit- Using Page Fragments

---
+++ src/main/java/me/jerilynmensah/springboot16/Springboot16Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.springboot16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16Application.class, args);
+	}
+}
